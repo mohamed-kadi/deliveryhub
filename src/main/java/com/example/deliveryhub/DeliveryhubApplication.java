211cff453ce5b513c7fb46c6a8dd8fc0ccfec546
mohamed.kadi <n.kadi63@gmail.com>
2025-05-29 09:53:12 -0400

    Initial Spring Boot project

---
+++ src/main/java/com/example/deliveryhub/DeliveryhubApplication.java
@@ -0,0 +1,13 @@
+package com.example.deliveryhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryhubApplication.class, args);
+	}
+
+}
